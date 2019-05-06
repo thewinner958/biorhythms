75b98e6bcc7024485f385dfacefb780640c45d5d
TheWinner <thewinner958@gmail.com>
2019-05-06 12:57:06 +0300

    added the code and changed the framwork to .NET Framework  4.5.2

--- Biorhythms/biorhythms.Designer.vb
+++ Biorhythms/biorhythms.Designer.vb
@@ -22,17 +22,110 @@ Partial Class frmBiorhythms
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.lblHead = New System.Windows.Forms.Label()
+        Me.lblDOB = New System.Windows.Forms.Label()
+        Me.BirthDay = New System.Windows.Forms.DateTimePicker()
+        Me.Physical = New System.Windows.Forms.RadioButton()
+        Me.Emotional = New System.Windows.Forms.RadioButton()
+        Me.Intellectual = New System.Windows.Forms.RadioButton()
+        Me.btnCalculate = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'lblHead
+        '
+        Me.lblHead.AutoSize = True
+        Me.lblHead.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHead.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.lblHead.Location = New System.Drawing.Point(30, 29)
+        Me.lblHead.Name = "lblHead"
+        Me.lblHead.Size = New System.Drawing.Size(191, 24)
+        Me.lblHead.TabIndex = 0
+        Me.lblHead.Text = "Biorhythms Calculator"
+        '
+        'lblDOB
+        '
+        Me.lblDOB.AutoSize = True
+        Me.lblDOB.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDOB.Location = New System.Drawing.Point(31, 76)
+        Me.lblDOB.Name = "lblDOB"
+        Me.lblDOB.Size = New System.Drawing.Size(103, 18)
+        Me.lblDOB.TabIndex = 1
+        Me.lblDOB.Text = "Date of Birth"
+        '
+        'BirthDay
+        '
+        Me.BirthDay.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.BirthDay.Location = New System.Drawing.Point(220, 74)
+        Me.BirthDay.Name = "BirthDay"
+        Me.BirthDay.Size = New System.Drawing.Size(114, 20)
+        Me.BirthDay.TabIndex = 2
+        '
+        'Physical
+        '
+        Me.Physical.AutoSize = True
+        Me.Physical.Location = New System.Drawing.Point(34, 159)
+        Me.Physical.Name = "Physical"
+        Me.Physical.Size = New System.Drawing.Size(92, 17)
+        Me.Physical.TabIndex = 3
+        Me.Physical.TabStop = True
+        Me.Physical.Text = "Physical State"
+        Me.Physical.UseVisualStyleBackColor = True
+        '
+        'Emotional
+        '
+        Me.Emotional.AutoSize = True
+        Me.Emotional.Location = New System.Drawing.Point(34, 219)
+        Me.Emotional.Name = "Emotional"
+        Me.Emotional.Size = New System.Drawing.Size(99, 17)
+        Me.Emotional.TabIndex = 4
+        Me.Emotional.TabStop = True
+        Me.Emotional.Text = "Emotional State"
+        Me.Emotional.UseVisualStyleBackColor = True
+        '
+        'Intellectual
+        '
+        Me.Intellectual.AutoSize = True
+        Me.Intellectual.Location = New System.Drawing.Point(34, 279)
+        Me.Intellectual.Name = "Intellectual"
+        Me.Intellectual.Size = New System.Drawing.Size(104, 17)
+        Me.Intellectual.TabIndex = 5
+        Me.Intellectual.TabStop = True
+        Me.Intellectual.Text = "Intellectual State"
+        Me.Intellectual.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(34, 319)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(90, 23)
+        Me.btnCalculate.TabIndex = 6
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
         '
         'frmBiorhythms
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(387, 386)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Intellectual)
+        Me.Controls.Add(Me.Emotional)
+        Me.Controls.Add(Me.Physical)
+        Me.Controls.Add(Me.BirthDay)
+        Me.Controls.Add(Me.lblDOB)
+        Me.Controls.Add(Me.lblHead)
         Me.Name = "frmBiorhythms"
         Me.Text = "Biorhythms"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents lblHead As Label
+    Friend WithEvents lblDOB As Label
+    Friend WithEvents BirthDay As DateTimePicker
+    Friend WithEvents Physical As RadioButton
+    Friend WithEvents Emotional As RadioButton
+    Friend WithEvents Intellectual As RadioButton
+    Friend WithEvents btnCalculate As Button
 End Class
--- Biorhythms/biorhythms.vb
+++ Biorhythms/biorhythms.vb
@@ -1,3 +1,30 @@
 ﻿Public Class frmBiorhythms
 
+    Dim Coeff As Integer
+    Dim Status As String
+    Dim Days As Integer
+    Dim D1 As Date
+    Dim D2 As Date
+
+    Private Sub Physical_CheckedChanged(sender As Object, e As EventArgs) Handles Physical.CheckedChanged
+        Coeff = 23
+        Status = "phisical"
+    End Sub
+
+    Private Sub Emotional_CheckedChanged(sender As Object, e As EventArgs) Handles Emotional.CheckedChanged
+        Coeff = 28
+        Status = "emotional"
+    End Sub
+
+    Private Sub Intellectual_CheckedChanged(sender As Object, e As EventArgs) Handles Intellectual.CheckedChanged
+        Coeff = 33
+        Status = "intellectual"
+    End Sub
+
+    Private Sub BtnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        D1 = BirthDay.Text
+        D2 = Today
+        Days = DateDiff(DateInterval.Day, D1, D2)
+        MsgBox("The percent of your " & Status & " state is " & Format(Math.Sin(360 * Days / Coeff), "00%"), 0, "Dr.Phil says...")
+    End Sub
 End Class
